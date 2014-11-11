3c72fde8d47593cc598ba179565f12a8382d5daa
abhiabhi15 <abhishek.agrawal15@yahoo.com>
2014-11-10 21:01:31 -0500

    Latest Prediction data and formatting changes

--- forest-cover/code/formatting.R
+++ forest-cover/code/formatting.R
@@ -8,4 +8,8 @@ writeData(output_data, "../results/svm/svm_output_en2_1.csv")
 ## Selecting output Data
 output_data <- getData("~/Desktop/output.csv")
 output_data <- output_data$prediction.Cover_Type.
-writeOutput(classifier="SVM",filename="svm_en2_output2.csv",output=output_data)
+writeOutput(classifier="SVM",filename="../results/svm/svm_check2.csv",output=rep(2, 565892))
+
+
+getData <- getData("../results/svm/svm_t7_output3.csv")
+table(getData$Cover_Type)/565892
