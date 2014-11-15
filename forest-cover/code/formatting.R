351d017f0eaddba9d8b122e5d2b1f0c87c0547e8
abhiabhi15 <abhishek.agrawal15@yahoo.com>
2014-11-14 20:50:49 -0500

    Modifying code for SVM and transformation

--- forest-cover/code/formatting.R
+++ forest-cover/code/formatting.R
@@ -12,4 +12,13 @@ writeOutput(classifier="SVM",filename="../results/svm/svm_t7_output5.csv",output
 
 
 getData <- getData("../results/svm/svm_t7_output3.csv")
-table(getData$Cover_Type)/565892
+table(getData$Cover_Type)/56589
+
+# Modifying Transform7 to transform8
+forest_data <- getData("../train/transform7.csv")
+CT <- as.character(forest_data$Cover_Type)
+split <- function(x){
+  strsplit(x, "[^[:digit:]]")[[1]][[2]]
+}
+forest_data$Cover_Type <- as.numeric(sapply(CT, split))
+writeData(data=forest_data, filename="../train/transform8.csv")
